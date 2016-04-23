96db0cabf6d42ad2af61e843180b2123e26ca7a4
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-04-23 15:33:01 -0500

    Conectando rutas y controladores

--- joinServer/app.js
+++ joinServer/app.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('static-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 var app = express();
 
@@ -21,8 +18,8 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+require('./routes')(app);
+require('./routes/profiles')(app, '/profile');
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
