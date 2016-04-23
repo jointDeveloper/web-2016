dec1a65154060c6b0f9625380b2babc0bf637944
Leiver Andres Campeon <leiverandres04p@hotmail.com>
2016-04-23 15:42:16 -0500

    fixed detalles menores

--- joinServer/app.js
+++ joinServer/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes')(app);
-require('./routes/profiles')(app, '/profile');
+require('./routes/profiles')(app, '/profiles');
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
