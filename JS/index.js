0131556fa109cc50283f945f61403cb3551baae5
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-05-18 17:59:53 -0500

    Refactoring de los script hover e index.js

--- JS/index.js
+++ JS/index.js
@@ -1,46 +1,6 @@
-(function(){
-
-  function Perfiles(){
-      window.location="HTML/perfiles.html";
-  }
-  function Comunidad(){
-      window.location="HTML/comunidad.html";
-  }
-  function About(){
-      window.location="HTML/about.html";
-  }
-  function Eventos(){
-      window.location="HTML/eventos.html";
-  }
-  function Portafolio(){
-      window.location="HTML/portafolio.html";
-  }
-  function Contacto(){
-      window.location="HTML/contacto.html";
-  }
-  function Apoyanos(){
-      window.location="HTML/apoyanos.html";
-  }
-  function Blog(){
-      window.location="HTML/blog.html";
-  }
-
-  var perfiles = document.getElementsByClassName('Perfiles'),
-      comunidad = document.getElementsByClassName('Comunidad'),
-      about = document.getElementsByClassName('About'), //quienes somos
-      eventos = document.getElementsByClassName('Eventos'),
-      portafolio = document.getElementsByClassName('Portafolio'),
-      contacto = document.getElementsByClassName('Contacto'),
-      apoyanos = document.getElementsByClassName('Apoyanos'),
-      blog = document.getElementsByClassName('Blog');
-
-  perfiles[0].addEventListener("click",Perfiles);
-  comunidad[0].addEventListener("click",Comunidad);
-  about[0].addEventListener("click",About);
-  eventos[0].addEventListener("click",Eventos);
-  portafolio[0].addEventListener("click",Portafolio);
-  contacto[0].addEventListener("click",Contacto);
-  apoyanos[0].addEventListener("click",Apoyanos);
-  blog[0].addEventListener("click",Blog);
-
-}())
+$(function() {
+  $('.link').click(function() {
+    var name = "HTML/" + $(this).attr('id') + ".html";
+    window.location = name;
+  });
+});
--- JS/scroll.js
+++ JS/scroll.js
@@ -1,46 +1,7 @@
 $(function() {
-	$("#inicio").hover(function() {
+	$(".scroll").hover(function() {
+		var id = "#nav-" + $(this).attr('id');
 		$('.active').removeClass('active');
-		$("#nav-inicio").addClass('active');
-	}, function() {});
-
-	$("#info").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-info").addClass('active');
-	}, function() {});
-
-	$("#aptitudes").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-aptitudes").addClass('active');
-	}, function() {});
-
-	$("#educacion").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-educacion").addClass('active');
-	}, function() {});
-
-	$("#cursos").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-cursos").addClass('active');
-	}, function() {});
-
-	$("#proyectosport").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-proyectosport").addClass('active');
-	}, function() {});
-
-	$("#pasatiempos").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-pasatiempos").addClass('active');
-	}, function() {});
-
-	$("#articulos").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-articulos").addClass('active');
-	}, function() {});
-
-	$("#contacto").hover(function() {
-		$('.active').removeClass('active');
-		$("#nav-contacto").addClass('active');
+		$(id).addClass('active');
 	}, function() {});
 });
