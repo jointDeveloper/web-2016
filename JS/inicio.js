b11e20b695cb937735cc439a2b3ce90922581466
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-07-17 15:00:59 -0500

    Añade pruebas de background-color en el nav

--- JS/inicio.js
+++ JS/inicio.js
@@ -1,7 +1,15 @@
 $(function() {
-  $("#nav-inicio").click(function() {
-    console.log("hola");
-        console.log($("a").attr("position"));
-
+  $(".navbar a").click(function() {
+    var id = $(this).attr('href');
+    var r = random(0, 255, true);
+    var g = random(0, 255, true);
+    var b = random(0, 255, true);
+    var a = random(0, 1, false);
+    $(id).css('background-color', `rgba(${r}, ${g}, ${b}, ${a})`);
   });
+
+  function random(min, max, opt) {
+    var ans = Math.random() * (max - min);
+    return (opt)? Math.floor(ans) : ans;
+  }
 });
