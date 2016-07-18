9ccf38d5d6a4046c2c02559c97c5a2481c9a4366
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-07-17 23:35:43 -0500

    inicio

--- JS/inicio.js
+++ JS/inicio.js
@@ -1,11 +1,25 @@
 $(function() {
   $(".navbar a").click(function() {
     var id = $(this).attr('href');
-    var r = random(0, 255, true);
+    //alert(id);
+    if (id == "#inicio") {
+      $(".navbar a").css('background', 'transparent');
+      $(".banner a").css('background', 'transparent');
+      $("#nombre").css('display', 'none');
+      $(this).css('color', '#fff');
+    }
+    else {
+      $(".navbar a").css('background', '#444');
+      $(".banner a").css('background', '#444');
+      $("#nombre").css('display', '');
+    }
+
+    /*var r = random(0, 255, true);
     var g = random(0, 255, true);
     var b = random(0, 255, true);
     var a = random(0, 1, false);
-    $(id).css('background-color', `rgba(${r}, ${g}, ${b}, ${a})`);
+    $(this).css('color', `rgba(${r}, ${g}, ${b}, ${a})`);*/
+
   });
 
   function random(min, max, opt) {
