cd08cfd33c2fdc922827f9cda4b5167e260fe1b8
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-07-21 19:27:52 -0500

    Form Contacto

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,6 +1,6 @@
 (function ($) {
 	$(function () {
-		$(".scroll").mouseenter(function () {
+		$(".panel").mouseenter(function () {
 			var id = "#nav-" + $(this).attr('id');
 			$('.active').removeClass('active');
 			$(id).addClass('active');
