210b2b36e2b1f942459dbdfd2697af9a52b13724
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-01-11 19:25:58 -0500

    Promera versión - Vista previa de perfiles

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,12 @@ const fs = require('fs');
 const profile = JSON.parse(fs.readFileSync('./data/profile.json'));
 
 exports.index = function(req, res) {
-  res.render('index', {profiles: profile.profiles});
+  const profiles = profile.profiles;
+  // console.log(profiles[0]);
+  // console.log(profiles[0]["john"].name);
+  // for (i in profiles[0]) {
+  //   console.log(i);
+  // }
+
+  res.render('index', {profiles: profiles[0]});
 };
