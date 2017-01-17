260e95354b88305859d4d0f19bc738a2b95d431e
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-01-16 20:16:47 -0500

    Se conserva versión 2016

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = new express.Router();
-const indexController = require('../controllers/index');
-
-/* GET home page. */
-router.get('/', indexController.index);
-
-module.exports = router;
